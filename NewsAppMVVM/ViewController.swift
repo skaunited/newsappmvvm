fef17a6e7a10bad300fc5d8b0a797fb3ebec4b09
Skander Bahri <s.bahri@workflow-dev.fr>
2020-11-30 13:10:38 +0100

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Skander Bahri on 30/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
